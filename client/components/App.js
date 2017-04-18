0e277f13a626e78fd7fc21b0cd50ac1ed284d179
david <ostermann.david@gmail.com>
2017-04-18 15:50:09 +0200

    config

--- client/components/App.js
+++ client/components/App.js
@@ -6,6 +6,6 @@ export default class App extends React.Component {
   }
 
   render() {
-    return (<div>MyComponent</div>);
+    return (<div>MyComddsdponent</div>);
   }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,46 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './client/index.html',
+  template: './index.html',
   filename: 'index.html',
   inject: 'body'
 });
 
 module.exports = {
-  entry: './client/index.js',
+  context: path.resolve(__dirname, 'client'),
+  entry: [
+    'react-hot-loader/patch',
+    // activate HMR for React
+
+    'webpack-dev-server/client?http://localhost:8080',
+    // bundle the client for webpack-dev-server
+    // and connect to the provided endpoint
+
+    'webpack/hot/only-dev-server',
+    // bundle the client for hot reloading
+    // only- means to only hot reload for successful updates
+    './index.js'
+  ],
   output: {
     path: path.resolve('dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '/'
   },
-  devtool: (process.env.NODE_ENV === 'production') ? 'cheap-source-map' : 'eval-source-map',
+  devtool: 'inline-source-map',
+  devServer: {
+    hot: true,
+    contentBase: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
   module: {
     loaders: [
       {test:/\.js$/, loader: 'babel-loader', exclude:/node_modules/}
     ]
   },
-  plugins: [HtmlWebpackPluginConfig]
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    HtmlWebpackPluginConfig
+  ]
 };
